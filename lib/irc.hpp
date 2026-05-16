ae00a44e6ed89cead3341a501596263d5997cec3
jomunoz42 <jpmunoz@sapo.pt>
2026-05-16 18:59:15 +0100

    removed includes.hpp, it was unecessary, project works properly

--- lib/irc.hpp
+++ lib/irc.hpp
@@ -2,7 +2,29 @@
 #ifndef IRC_HPP
 # define IRC_HPP
 
-# include "includes.hpp"
+# include <map>
+# include <vector>
+# include <string>
+# include <poll.h>
+# include <cerrno>
+# include <cstring>
+# include <cstdlib>
+# include <fcntl.h>
+# include <netdb.h>
+# include <sstream>
+# include <signal.h>
+# include <unistd.h>
+# include <iostream>
+# include <iterator>
+# include "Server.hpp"
+# include "Client.hpp"
+# include <sys/stat.h>
+# include "Channel.hpp"
+# include "defines.hpp"
+# include <sys/types.h>
+# include <arpa/inet.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
 
 std::vector<std::string> split(std::string str, std::string del);
 
